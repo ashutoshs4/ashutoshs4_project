2e30d660f3230912305e7a4cd84699fe26becfbe
Ashutoshs4 <as760059@gmail.com>
2022-12-16 03:54:41 +0530

    dev1

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -34,6 +34,7 @@ function Registration() {
 
                 <Row className="mb-3">
                     <Button variant="primary" type="button">
+                        {/* dev 1 */}
                         Registration
                     </Button>
                 </Row>
