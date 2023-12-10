ab2003f71a7ace4549ef166cc820dd9ceea6adc0
Ashutoshs4 <as760059@gmail.com>
2023-12-10 17:16:55 +0530

    Remove text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,9 +3,6 @@ import React from 'react'
 function About(){
     return (
         <>
-            <h1>Hello About Dev</h1>
-            <h1>Hello Secnd Line</h1>
-            <h1>Hello Ashutosh</h1>
             <h1>Hheheeh</h1>
         </>
     );
