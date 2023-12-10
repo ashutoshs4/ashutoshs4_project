7cb2628374c76d40669febabcc578e766c273bc9
Ashutoshs4 <as760059@gmail.com>
2023-12-10 15:42:42 +0530

    About second line added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 
 function About(){
     return (
-        <div>
-            <h1>About</h1>
-        </div>
+        <h1>Hello About Dev</h1>
+        <h1>Hello Secnd Line</h1>
     );
 }
 
